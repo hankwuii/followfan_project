a1ab1301fba5ffe8d4d09103a1da915620c7cf70
hankwuii <qazx60610@gmail.com>
2022-09-07 12:43:16 +0800

    v2_9/7

---
+++ src/service_server.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include "motor_server/mysrv.h"
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+float motor_step;
+
+bool division_Callback(motor_server::mysrv::Request  &req, motor_server::mysrv::Response &res){
+   motor_step = req.angle / 1.8;
+   cout<<"step = "<< motor_step <<endl;
+}
+
+int main(int argc, char** argv){
+   ros::init(argc, argv, "service_server"); 
+   ros::NodeHandle n; 
+   ros::ServiceServer service = n.advertiseService("mysrv", division_Callback); 
+
+   ros::spin();
+
+   return 0;
+}
